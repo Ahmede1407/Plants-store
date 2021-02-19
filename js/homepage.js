42612c402c3ffd677558afd56dd41f99bc1c0854
Ahmed Elsayed <ahmed.mano.am68@gmail.com>
2021-02-19 23:30:15 +0200

    Finished Home page with all factionality and responsive and has beautiful features.

--- js/homepage.js
+++ js/homepage.js
@@ -3,6 +3,20 @@ import CreatePlants from './createPlant.js';
 // show loader when loading
 window.addEventListener('load', () => document.querySelector('.loader').classList.add('hideLoader'))
 
+
+// Scroll Top
+function scrollTop(){
+  let scrollTop = document.querySelector('#scrollTop');
+
+  if ( this.scrollY >= 560){
+    scrollTop.classList.add('show-arrow');
+  }else{
+    scrollTop.classList.remove('show-arrow');
+  }
+}
+window.addEventListener('scroll', scrollTop);
+
+
 // index.html
 const DisplayFeaturedPlants = ((plants) => {
     const featuredPlants = plants.featuredPlants;
